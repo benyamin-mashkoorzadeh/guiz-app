43aeef0621e15125919f2b5e3a4d96ae9d9ced31
Benyamin <benyamin@Benyamins-Air.fritz.box>
2024-09-21 16:42:00 +0200

    modify quiz/layout.js generateStaticParams

--- src/app/[lang]/quiz/layout.js
+++ src/app/[lang]/quiz/layout.js
@@ -1,6 +1,14 @@
 import Quiz from "@/src/app/[lang]/quiz/page";
 import {getLangs} from "@/src/app/[lang]/langs";
 
+export async function generateStaticParams() {
+    const langs = ['en-us', 'fa', 'en', 'fa-ir']
+
+    console.log(langs)
+
+    return langs.map((lang) => (lang))
+}
+
 export default async function Layout({params}) {
     const dict = await getLangs(params.lang)
     return (
