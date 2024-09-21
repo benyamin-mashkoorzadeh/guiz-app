1407dbfa8a21246d5e70abac0f72921791f16aca
Benyamin <benyamin@Benyamins-Air.fritz.box>
2024-09-21 16:37:15 +0200

    modify photo/page.js generateStaticParams

--- src/app/[lang]/about/photo/page.js
+++ src/app/[lang]/about/photo/page.js
@@ -1,5 +1,11 @@
 import Frame from "@/src/components/modal/Frame";
 
+export async function generateStaticParams() {
+    const langs = ['en-us', 'fa', 'en', 'fa-ir']
+
+    return langs.map((lang) => (lang))
+}
+
 export default function PhotoPage() {
 
     const photo = "https://picsum.photos/id/237/200/300"
